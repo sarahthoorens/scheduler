5ec19df024052f6fabf8be488828aa6ac47f495e
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-06 20:33:39 -0500

    added functions to set and get appointment at one time block

--- script.js
+++ script.js
@@ -4,5 +4,32 @@ console.log(dayjs)
 currentTime = dayjs().format('MMMM DD, YYYY HH:mm:ss')
 console.log(currentTime)
 dateTime.textContent = currentTime
+let calendarEntry= document.getElementById('input')
+let saveBtn = document.getElementById('calendar-btn')
 
 
+// Save new and render existing appointments upon page load
+function saveLastEntry() {
+    localStorage.setItem('appointment', calendarEntry.value);
+    console.log(calendarEntry)
+}
+
+function renderAllEntries () {
+    var lastEntries = localStorage.getItem('appointment');
+    if (lastEntries !== null) {
+        console.log(lastEntries);
+        calendarEntry.value = lastEntries;
+    } else {
+        return;
+    }
+}
+saveBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    saveLastEntry();
+    renderAllEntries();
+});
+
+function init() {
+    renderAllEntries();
+}
+init();
